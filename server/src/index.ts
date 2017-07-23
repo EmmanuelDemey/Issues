3bc35749e5368bb7107e7f9967f435fe804f5b41
Emmanuel DEMEY <demey.emmanuel@gmail.com>
2017-07-23 09:45:48 +0200

    Add 500/404 http code

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,13 +1,11 @@
 import * as express from "express";
 import * as bodyParser from "body-parser";
 import * as mongoose from "mongoose";
-import { IssueModel, Issue } from './model/issue';
-
+import { IssueModel, Issue } from "./model/issue";
 
 const app = express();
 const issues = require("./resources/issues");
 const uuidv4 = require("uuid/v4");
-
 
 class Server {
   private app: express.Application;
@@ -19,28 +17,49 @@ class Server {
     this.routes();
   }
 
-  private initMongoDB(){
-    mongoose.connect('mongodb://localhost/issues', {useMongoClient: true}, (err) => {
-      if(err){
-        console.log(err)
+  private initMongoDB() {
+    mongoose.connect(
+      "mongodb://localhost/issues",
+      { useMongoClient: true },
+      err => {
+        if (err) {
+          console.log(err);
+        }
       }
-    });
+    );
   }
 
   private routes() {
-    this.app.get("/issues", function(req: express.Request, res: express.Response) {
-      issues.getAll(parseInt(req.query.page), parseInt(req.query.size), (err: Error, response: Issue[]) => {
-        res.json(response);
-      });
+    this.app.get("/issues", function(
+      req: express.Request,
+      res: express.Response
+    ) {
+      issues.getAll(
+        parseInt(req.query.page),
+        parseInt(req.query.size),
+        (err: Error, response: Issue[]) => {
+          if (err) {
+            res.sendStatus(500);
+          } else {
+            res.json(response);
+          }
+          res.json(response);
+        }
+      );
     });
 
     this.app.get("/issues/:id", function(
       req: express.Request,
       res: express.Response
     ) {
-
       issues.getById(req.params.id, (err: Error, response: Issue) => {
-        res.json(response);
+        if (err) {
+          res.sendStatus(500);
+        } else if(!response){
+          res.sendStatus(404);
+        } else {
+          res.json(response);
+        }
       });
     });
 
@@ -48,14 +67,29 @@ class Server {
       req: express.Request,
       res: express.Response
     ) {
-      issues.update(req.params.id, req.body, (err: Error, response: Issue[]) => {
-        res.sendStatus(200);
-      })
+      issues.update(
+        req.params.id,
+        req.body,
+        (err: Error, response: Issue[]) => {
+          if (err) {
+            res.sendStatus(500);
+          } else {
+            res.sendStatus(200);
+          }
+        }
+      );
     });
 
-    this.app.post("/issues", function(req: express.Request, res: express.Response) {
-      issues.save(req.body, () => {
-        res.sendStatus(201);
+    this.app.post("/issues", function(
+      req: express.Request,
+      res: express.Response
+    ) {
+      issues.save(req.body, (err: Error) => {
+        if (err) {
+          res.sendStatus(500);
+        } else {
+          res.sendStatus(201);
+        }
       });
     });
 
@@ -63,10 +97,13 @@ class Server {
       req: express.Request,
       res: express.Response
     ) {
-      issues.delete(req.params.id, () => {
-        res.sendStatus(200);
-      })
-      
+      issues.delete(req.params.id, (err: Error) => {
+        if (err) {
+          res.sendStatus(500);
+        } else {
+          res.sendStatus(200);
+        }
+      });
     });
 
     this.app.listen(3000, function() {
